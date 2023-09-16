aeb7a67ad0bfcefae5ff6b67ee1b74e32fab65db
Lucy Steckler <lucy.steckler@gmail.com>
2023-09-15 22:18:08 -0600

    made task editable

--- todo-app/src/app.js
+++ todo-app/src/app.js
@@ -4,6 +4,16 @@
 
   let addBtn = document.getElementById("addTaskBtn")
   let list = document.getElementById("todoList")
+  let header = document.getElementById("header")
+  
+  
+  const check = document.getElementById("check")
+
+  function checkPending() {
+    const number = document.getElementById("todoList").childElementCount
+    const counterText = document.getElementById("counter")
+    counterText.textContent =  number
+  }
 
  
   function newTask() {
@@ -11,43 +21,62 @@
    
     const taskWrapper = document.createElement("span")
     task.setAttribute('id', 'task')
+    task.setAttribute("contenteditable", "true")
     const newInput = document.getElementById("myInput").value
    task.innerHTML = newInput
+ 
    
    
     list.appendChild(taskWrapper)
     taskWrapper.appendChild(task)
+  
 
     taskWrapper.addEventListener("click", strike = () => {
       task.classList.toggle("strike")
+   
     })
     document.getElementById("myInput").value = "";
+
 
 
 
     const wrapper = document.createElement("SPAN");
     wrapper.innerHTML = "\u00D7";
     wrapper.className = "close";
+    
   
     task.appendChild(wrapper);
   wrapper.addEventListener("click", close = () => {
     task.style.display = "none"
+    
 
 
   })
 
-   
+ 
   }
  const clearButton = document.getElementById("clearDone")
  clearButton.addEventListener("click", clearDone)
  function clearDone() {
   if (document.getElementById('task')) {
     const element = document.getElementsByClassName("strike")
+ 
     for (i = 0; i < element.length; i++)
     element[i].hidden = true;
+    
+
+
   }
+
  
  }
+
+
+function edit() {
+  task.setAttribute("contenteditable", "true")
+}
+
+
 
   
 
@@ -59,3 +88,24 @@
 //    completeTask.remove()
 
 //    }
+
+// function removeLi(task) {
+//   if (window.getComputedStyle(task).display === "none")
+//   task.remove()
+
+// }
+// function checkCounter() {
+//   removeLi(document.getElementById('task'))
+
+//   const listItems= Array.from(document.getElementById('todoList').children)
+
+//   for (i=0; i > listItems.length; i++) {
+//     if (window.getComputedStyle(listItems[i]).display === "none") {
+//       listItems[i].pop()
+//     } else {
+      
+//     }
+//   }
+//   console.log(listItems.length)
+
+// }
